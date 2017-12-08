7226ae888ceda364db9e6aff7f7544fc58bf831b
johirbuet <johirbuet@gmail.com>
2017-12-08 11:27:31 -0600

    Using count()

--- src/List/ParallelStreamPerformance.java
+++ src/List/ParallelStreamPerformance.java
@@ -9,10 +9,10 @@ import java.util.Random;
  * @author johir
  * The method count is syncronized because it will be used by parallelstream
  * Another way can be to use AtomicInteger
- *
+ * Instead of using synchronized method we can use count() on agrreagator that is built in.
  */
 public class ParallelStreamPerformance {
-	static int totaleven = 0;
+	static long totaleven = 0;
 	public static void main(String[] args) {
 		List<Integer> randomList = new ArrayList<>();
 		Random rnd = new Random();
@@ -23,7 +23,7 @@ public class ParallelStreamPerformance {
 		
 		long s1 = System.currentTimeMillis();
 		
-		randomList.stream().filter(e -> e%2 ==0).forEach(e -> count());
+		totaleven = randomList.stream().filter(e -> e%2 ==0).count();
 		System.out.println("Even: "+totaleven);
 		long e1 = System.currentTimeMillis();
 		System.out.println("Time: "+(e1 - s1));
@@ -31,7 +31,7 @@ public class ParallelStreamPerformance {
 		totaleven = 0;
 		long s2 = System.currentTimeMillis();
 		
-		randomList.parallelStream().filter(e -> e%2 ==0).forEach(e -> count());
+		totaleven = randomList.parallelStream().filter(e -> e%2 ==0).count();
 		System.out.println("Even: "+totaleven);
 		long e2 = System.currentTimeMillis();
 		System.out.println("Time: "+(e2 - s2));
